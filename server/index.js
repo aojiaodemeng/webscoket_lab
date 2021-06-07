29a7cdfdf8abbe66671ead3403a0146f1666bb5f
chenxinyi <chenxinyi@vikadata.com>
2021-06-07 18:50:43 +0800

    feat: 简单的ws消息广播

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8880 });
+
+wss.on("connection", function connection(ws) {
+  console.log("connection");
+  ws.on("message", function (message) {
+    console.log("received: %s", message);
+    ws.send(message);
+    wss.clients.forEach((client) => {
+      if (ws !== client && client.readyState === WebSocket.OPEN) {
+        client.send("server:" + message);
+      }
+    });
+  });
+});
