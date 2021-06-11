c59076e5c23dd60587ac9bad99272e16b5dd8275
chenxinyi <chenxinyi@vikadata.com>
2021-06-11 16:00:09 +0800

    feat: 统计在线人数，退出聊天室人数处理

--- server/index.js
+++ server/index.js
@@ -2,14 +2,36 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8880 });
 
+let num = 0;
 wss.on("connection", function connection(ws) {
-  console.log("connection");
   ws.on("message", function (message) {
     console.log("received: %s", message);
+    const msgObj = JSON.parse(message);
+    if (msgObj.event === "enter") {
+      ws.name = msgObj.message;
+      num++;
+    }
     // ws.send(message);
     wss.clients.forEach((client) => {
-      if (ws !== client && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name;
+        msgObj.num = num;
+        client.send(JSON.stringify(msgObj));
+      }
+    });
+  });
+
+  ws.on("close", function () {
+    if (ws.name) {
+      num--;
+    }
+    let msgObj = {};
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        msgObj.name = ws.name;
+        msgObj.num = num;
+        msgObj.event = "out";
+        client.send(JSON.stringify(msgObj));
       }
     });
   });
