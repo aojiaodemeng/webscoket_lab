f1a9ff4522a87d88703596b952fa20cf518891fc
chenxinyi <chenxinyi@vikadata.com>
2021-06-11 11:35:39 +0800

    feat: 进入聊天室欢迎语

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ wss.on("connection", function connection(ws) {
   console.log("connection");
   ws.on("message", function (message) {
     console.log("received: %s", message);
-    ws.send(message);
+    // ws.send(message);
     wss.clients.forEach((client) => {
       if (ws !== client && client.readyState === WebSocket.OPEN) {
-        client.send("server:" + message);
+        client.send(message);
       }
     });
   });
