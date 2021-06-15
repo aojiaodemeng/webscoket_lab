0d1b3396759052f82d932304fea00176c644ad6b
chenxinyi <chenxinyi@vikadata.com>
2021-06-15 13:36:10 +0800

    feat: 鉴权

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+const http = require("http");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8880 });
-
-let num = 0;
+const wss = new WebSocket.Server({ noServer: true });
+const server = http.createServer();
+const jwt = require("jsonwebtoken");
 let group = {};
 
 // 多聊天室的功能
@@ -19,6 +20,32 @@ wss.on("connection", function connection(ws) {
       } else {
         group[ws.roomId]++;
       }
+    }
+
+    // 鉴权
+    if (msgObj.event === "auth") {
+      jwt.verify(msgObj.message, "123456781", (err, decode) => {
+        if (err) {
+          console.log("auth error");
+          return;
+        } else {
+          // 鉴权通过
+          console.log(decode);
+          ws.isAuth = true;
+          return;
+        }
+      });
+    }
+
+    // 拦截非鉴权的请求
+    if (!ws.isAuth) {
+      ws.send(
+        JSON.stringify({
+          event: "noauth",
+          message: "please auth again",
+        })
+      );
+      return;
     }
     // ws.send(message);
     wss.clients.forEach((client) => {
@@ -44,4 +71,23 @@ wss.on("connection", function connection(ws) {
       }
     });
   });
+});
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  // This function is not defined on purpose. Implement it with your own logic.
+  // authenticate(request, (err, client) => {
+  //   if (err || !client) {
+  //     socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+  //     socket.destroy();
+  //     return;
+  //   }
+  console.log(request.headers);
+
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit("connection", ws, request);
+  });
+  // });
+});
+server.listen(8880, function () {
+  console.log("Listening on http://localhost:8880");
 });
